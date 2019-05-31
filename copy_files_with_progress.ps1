924205053336608f675092702fa8f2f585fc56b3
edgyduck <44840663+edgyduck@users.noreply.github.com>
2019-05-31 12:09:22 +0300

    Does what name says

---
+++ copy_files_with_progress.ps1
@@ -0,0 +1,19 @@
+﻿$dir = "E:\DCIM\*"
+$ext = "*.mov"
+$destination = "D:\Video\snow\kelias"
+
+$fileCount = (Get-ChildItem $dir -Recurse -Include $ext).count
+
+Write-Host "Found $fileCount files"
+
+$files = Get-ChildItem $dir -Recurse -Include $ext
+
+$i = 1
+
+$files | %{ 
+
+    
+    Write-Host "copying $_, file $i/$fileCount"; $i++
+    Copy-Item $_ -Destination $destination -Force
+    
+}
